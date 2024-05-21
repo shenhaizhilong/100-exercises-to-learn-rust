04660b5e8a750c14238b6488a127a5ae0022dab7
shenhaizhilong <shenhaizhilong@users.noreply.github.com>
2024-05-22 00:44:26 +0800

    feat(rust): rust exercises

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,74 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    name: String,
+    quantity: u32,
+    price: u32,
+}
+
+impl Order {
+    pub fn new(name: String, quantity: u32, price: u32) -> Order {
+        Self::check_name(&name);
+        Self::check_quantity(quantity);
+        Self::check_price(price);
+        Order {
+            name: name,
+            quantity: quantity,
+            price: price,
+        }
+    }
+
+    pub fn check_name(name: &String) {
+        if (name.is_empty() || name.len() > 300) {
+            panic!("invalid name");
+        }
+    }
+
+    pub fn check_quantity(quantity: u32) {
+        if (quantity <= 0) {
+            panic!("invalid quantity");
+        }
+    }
+
+    pub fn check_price(price: u32) {
+        if (price <= 0) {
+            panic!("invalid price");
+        }
+    }
+
+    pub fn product_name(&self) -> &String {
+        return &self.name;
+    }
+
+    pub fn quantity(&self) -> &u32 {
+        return &self.quantity;
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        return &self.price;
+    }
+
+    pub fn total(&self) -> u32 {
+        return self.price * self.quantity;
+    }
+
+    pub fn set_product_name(&mut self, name: String) -> &Self {
+        Self::check_name(&name);
+        self.name = name;
+        return self;
+    }
+
+    pub fn set_quantity(&mut self, quantity: u32) -> &Self {
+        Self::check_quantity(quantity);
+        self.quantity = quantity;
+        return self;
+    }
+
+    pub fn set_unit_price(&mut self, price: u32) -> &Self {
+        Self::check_price(price);
+        self.price = price;
+        return self;
+    }
+}

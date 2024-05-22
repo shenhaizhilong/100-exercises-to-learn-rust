5044f0dbf8d84ba4b65d7650e12cd0aa1ac86ab4
shenhaizhilong <shenhaizhilong@users.noreply.github.com>
2024-05-23 00:29:25 +0800

    feat(rust): rust exercises

--- exercises/05_ticket_v2/08_error_enums/src/lib.rs
+++ exercises/05_ticket_v2/08_error_enums/src/lib.rs
@@ -1,14 +1,31 @@
+use crate::TicketNewError::{DescriptionErr, TitleErr};
+
 // TODO: Use two variants, one for a title error and one for a description error.
 //   Each variant should contain a string with the explanation of what went wrong exactly.
 //   You'll have to update the implementation of `Ticket::new` as well.
-enum TicketNewError {}
+#[derive(Debug)]
+enum TicketNewError {
+    TitleErr { msg: String },
+    DescriptionErr { msg: String },
+}
 
 // TODO: `easy_ticket` should panic when the title is invalid, using the error message
 //   stored inside the relevant variant of the `TicketNewError` enum.
 //   When the description is invalid, instead, it should use a default description:
 //   "Description not provided".
 fn easy_ticket(title: String, description: String, status: Status) -> Ticket {
-    todo!()
+    let v = Ticket::new(title.clone(), description, status.clone());
+    match v {
+        Ok(ticket) => { return ticket; }
+        Err(err) => {
+            match err {
+                TitleErr { msg } => { panic!("{}", msg) }
+                DescriptionErr { msg } => {
+                    return Ticket::new(title, "Description not provided".to_string(), status).unwrap();
+                }
+            }
+        }
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -32,16 +49,16 @@ impl Ticket {
         status: Status,
     ) -> Result<Ticket, TicketNewError> {
         if title.is_empty() {
-            return Err("Title cannot be empty".to_string());
+            return Err(TitleErr { msg: "Title cannot be empty".to_string() });
         }
         if title.len() > 50 {
-            return Err("Title cannot be longer than 50 characters".to_string());
+            return Err(TitleErr { msg: "Title cannot be longer than 50 characters".to_string() });
         }
         if description.is_empty() {
-            return Err("Description cannot be empty".to_string());
+            return Err(DescriptionErr { msg: "Description cannot be empty".to_string() });
         }
         if description.len() > 500 {
-            return Err("Description cannot be longer than 500 characters".to_string());
+            return Err(DescriptionErr { msg: "Description cannot be longer than 500 characters".to_string() });
         }
 
         Ok(Ticket {
